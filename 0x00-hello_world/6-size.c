942e9df70ae4632a07c24f719d4d2ff6b1d5e3db
brian ndilo <b_munguti@yahoo.com>
2023-03-16 07:08:48 -0700

    Size is not all

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Function
+ *
+ * Return: 0 for succes
+ *
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(S)", sizeof(char));
+	printf("Size of an int: %lu bytre(S)", sizeof(int));
+	printf("size of a long int: %lu bytes(S)", sizeof(long int));
+	printf("size of a long long int: %lu byte(S)", sizeof(long long int));
+	printf("Soze of a float: %lu byte(S)", sizeof(float));
+	return (0);
+}
