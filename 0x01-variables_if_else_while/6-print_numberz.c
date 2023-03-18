1cf2b5981e3136198e5f8520441f928303e96261
brian ndilo <b_munguti@yahoo.com>
2023-03-17 18:58:39 -0700

    Nemberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Funtion
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i = '0');
+	putchar('\n');
+	return (0);
+}
