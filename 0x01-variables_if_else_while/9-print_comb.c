50dfd7eac321fd7a003289fd2adb83660bc89678
brian ndilo <b_munguti@yahoo.com>
2023-03-19 02:00:46 -0700

    Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 0 ; i <=9 ; i++)
 	{
 		putchar(i + '0');
-		putchar(' ');
 		if (i != 9)
 		{
 			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
