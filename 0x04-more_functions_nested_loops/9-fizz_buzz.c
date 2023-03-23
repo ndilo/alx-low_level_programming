92466380ae408f06f9c1623ea454fe2d1f6ae99f
brian ndilo <b_munguti@yahoo.com>
2023-03-23 12:54:05 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
 		{
@@ -31,7 +31,9 @@ int main(void)
 			printf("%d", i);
 		}
 		if (i > 100)
+		{
 			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);
