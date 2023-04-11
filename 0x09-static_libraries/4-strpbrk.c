10d14d8da96d40321f2dd7fec1093d3c4b6d12cc
brian ndilo <b_munguti@yahoo.com>
2023-04-10 19:09:47 -0700

    static

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes
+ *
+ * @s:first occurrence in the string
+ *
+ * @accept: matches one of the bytes, or @NULL if no such byte
+ *
+ * Return: a pointer to the byte
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int index;
+		while (*s)
+		{
+			for (index = 0; accept[index]; index++)
+			{
+				if (*s == accept[index])
+					return (s);
+			}
+			s++;
+		}
+	return ('\0');
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  _abs - Computes the absolute value of an integer
+ * @n: number computed
+ * Return: Absolute number
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
