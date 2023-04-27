72d12326d8e049bff25c49872d5431f58123c347
brian ndilo <b_munguti@yahoo.com>
2023-04-27 08:28:32 -0700

    New node at head of list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,18 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
- list_t *add;
- unsigned int len = 0;
+	list_t *add;
+	unsigned int len = 0;
 
- while (str[len])
- len++;
+	while (str[len])
+		len++;
 
- add = malloc(sizeof(list_t));
- if (!add)
- return (NULL);
-
- add->str = strdup(str);
- add->len = len;
- add->next = (*head);
- (*head) = add;
-
- return (*head);
+	add = malloc(sizeof(list_t));
+	if (!add)
+		return (NULL);
+	add->str = strdup(str);
+	add->len = len;
+	add->next = (*head);
+	(*head) = add;
+	return (*head);
 }
