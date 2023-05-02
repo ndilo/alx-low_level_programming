acaa4e9d42fb8373214df23411131b7b22650944
brian ndilo <b_munguti@yahoo.com>
2023-05-02 14:06:07 -0700

    Reverse

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -18,7 +18,7 @@ listint_t *reverse_listint(listint_t **head)
 	while (*head)
 	{
 		next = (*head)->next;
-		(*head)->next = previous;
+		(*head)->next = prev;
 		prev = *head;
 		*head = next;
 	}
