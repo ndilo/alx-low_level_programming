53daab26bc1ce28ac9e593cb1c7a199178ad0171
brian ndilo <b_munguti@yahoo.com>
2023-05-04 16:25:59 -0700

    number of bits you would need to flip to get from one number to another.

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int i, count = 0;
 	unsigned long int diff = n ^ m;
 
-	for (i = (sizeof(unsigned long int) * 8 -1); i >= 0; i--)
+	for (i = (sizeof(unsigned long int) * 8 - 1); i >= 0; i--)
 	{
 		if ((diff >> i) & 1)
 			count++;
