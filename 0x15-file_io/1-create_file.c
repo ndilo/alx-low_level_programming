e46965e7db49823fa1ebd6f1cdee7eeed9874be8
brian ndilo <b_munguti@yahoo.com>
2023-05-08 14:15:49 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	w = write(file, text_content, len);
 
 	if (file == -1 || w == -1)
 		return (-1);
