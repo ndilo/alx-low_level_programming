c4c2814bc52e4be3f92ea4b7ee9b01c2b3a20dcd
ndilo <b_munguti@yahoo.com>
2023-07-10 12:28:46 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,50 +1,64 @@
-#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include "main.h"
+
+unsigned long int _strlen(char *str);
 
 /**
- * _strlen - finds the length of a string
- * @str: pointer to the string
+ * create_file - creates a new file
+ * @filename: Name of the text file to be created
+ * @text_content: Text that should be placed in the new file
  *
- * Return: ;ength of the string
- *
- **/
-
-size_t _strlen(char *str)
-{
-	size_t j;
-
-	for (j = 0; str[j]; j++)
-		;
-	return (j);
-}
-
-/**
- * create_file - creates a file
- * @filename: name of the file to create
- * @text_content: NULL terminated string to write to the file
- *
- * Return: 1 on success, -1 on fail
- *
- **/
+ * Return: 1 if success or -1 if failure.
+ */
 
 int create_file(const char *filename, char *text_content)
 {
-	int file_d;
-	ssize_t length = 0;
+	int file_d, bytes_written;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	file_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	file_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file_d == -1)
 		return (-1);
-	if (text_content != NULL)
-		length = write(file_d, text_content, _strlen(text_content));
-	close(file_d);
-	if (length == -1)
-		return (-1);
-	return (1);
 
+	if (text_content)
+	{
+		bytes_written = write(file_d, text_content, _strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(file_d);
+			return (-1);
+		}
+
+		if ((size_t)bytes_written != _strlen(text_content))
+		{
+			close(file_d);
+			return (-1);
+		}
+	}
+
+	close(file_d);
+	return (1);
+}
+
+/**
+ * _strlen - Finds the length of a non-empty array.
+ * @str: Array to be counted
+ *
+ * Return: Number of characters.
+ */
+
+unsigned long int _strlen(char *str)
+{
+	unsigned long int length;
+
+	for (length = 0; str[length]; length++)
+	{};
+
+	return (length);
+}
